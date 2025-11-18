17d1b8814cfedc009758dab88168baf869f27df9
ferdibayram <ferdigit@gmail.com>
2025-11-18 08:08:32 +0300

    cont

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using Struct;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student stu1 = new Student();
+        stu1.Id = 1234;
+        stu1.Name = "Ferdi";
+        stu1.SurName = "Bayram";
+        stu1.Gender = true;
+
+        Console.WriteLine($"No: {stu1.Id} " +
+            $"{stu1.Name}-{stu1.SurName}-{(stu1.Gender ? "Erkek" : "Kadın")}");
+        Console.WriteLine("********************************");
+        Student stu2 = new Student(678, "Ayşe", "Ayık", false);
+        Console.WriteLine($"No: {stu2.Id} " +
+            $"{stu2.Name}-{stu2.SurName}-{(stu2.Gender ? "Erkek" : "Kadın")}");
+    }
+}
---
+++ Struct/Struct/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    
+    struct Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public bool Gender { get; set; }
+
+        public Student(int id, string name, string surname,bool gender )
+        {
+            Id = id;
+            Name = name;
+            SurName = surname;
+            Gender = gender;
+        }
+    }
+}
